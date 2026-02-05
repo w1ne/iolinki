8bbdd4c04c1ae20d89e3f4c83b77a3bed3c3eddc
w1ne <14119286+w1ne@users.noreply.github.com>
2026-02-05 23:47:53 +0100

    chore: Remove accidental build artifacts and update gitignore

--- build_strict/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/andrii/Projects/iolinki/tests
-# Build directory: /home/andrii/Projects/iolinki/build_strict/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
